cf3d3a51e317fb855b9d909c2c2da5f849f8a80f
Vynlar <lightningzygella@gmail.com>
2017-11-15 11:57:51 -0700

    made the game work and added boss door

---
+++ Carnival Game/Assets/Scripts/BossDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDoor : MonoBehaviour {
+
+    private int lockStatus = 0;
+
+    public Dialogue dialogue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void incrementStatus()
+    {
+        lockStatus++;
+        dialogue.SetDialogState("stage" + lockStatus);
+    }
+}
