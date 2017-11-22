280d34403625c7876e923a525babc473e134ff35
Vincent Crespin <crespinv@unm.edu>
2017-11-22 13:45:46 -0700

    Forgot to add the files and i added a background to the main menu

---
+++ Carnival Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    public bool isStart;
+    public bool isQuit;
+    private void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene("SideScrollerPrototype");
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Carnival Game/Assets/Scripts/MouseHoverText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoverText : MonoBehaviour
+{
+    
+    Renderer textColor;
+    // Use this for initialization
+
+    void Start()
+    {
+        textColor = GetComponent<Renderer>();
+        textColor.material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        textColor.material.color = Color.blue;
+    }
+    private void OnMouseExit()
+    {
+        textColor.material.color = Color.white;
+    }
+}
