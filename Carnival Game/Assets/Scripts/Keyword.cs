e1ea5c5793498ff0d74df416a41fa2a1b3885be5
Alex Booher <alex.booher@yahoo.com>
2017-11-23 02:30:51 -0700

    Completely changed the word puzzle interface

--- Carnival Game/Assets/Scripts/Keyword.cs
+++ Carnival Game/Assets/Scripts/Keyword.cs
@@ -6,36 +6,121 @@ using UnityEngine.UI;
 
 public class Keyword : MonoBehaviour {
 
+    // The word that is needed to trigger success events
     public string expectedWord;
 
+    // Events to trigger during succeed or fail
     public UnityEvent succeed;
     public UnityEvent fail;
-    public RectTransform inputPanel;
 
-    private InputField inputField;
-    private Text placeHolderText;
+    // References to important UI elements
+    public RectTransform basePanel;
+    public Transform charPanelGroup;
+    public GameObject charPanelPrefab;
+
+    // List of text objects in character panels
+    private List<GameObject> charPanels;
+    private List<Text> charPanelText;
+
+    // Other important elements that we find on start
     private Button doneButton;
+
+    private string enteredKeyword;
+    private int currentCharIndex = 0;
+    private bool isTyping = false;
 
 	// Use this for initialization
 	void Start () {
-        inputPanel.gameObject.SetActive(false);
-        inputField = inputPanel.transform.Find("InputPanel/InputField").gameObject.GetComponent<InputField>();
-        placeHolderText = inputField.transform.Find("Placeholder").gameObject.GetComponent<Text>();
-        doneButton = inputPanel.transform.Find("DoneButton").GetComponent<Button>();
+
+        charPanels = new List<GameObject>();
+        charPanelText = new List<Text>();
+
+        // make the panel invisable on start
+        basePanel.gameObject.SetActive(false);
+
+        // Clean out any existing character panels
+        ClearCharPanels();
+
+        // Get the references to the UI elements that we need
+        doneButton = basePanel.transform.Find("DoneButton").GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update () {
-		
+
+        if(!isTyping)
+        {
+            return;
+        }
+
+		if(Input.anyKeyDown)
+        {
+            char inChar = '=';
+            if(Input.inputString != "")
+            {
+                inChar = Input.inputString.ToCharArray()[0];
+            }
+
+            // If the input character was a letter
+            if((inChar >= 'a' && inChar <= 'z') || (inChar >= 'A' && inChar <= 'Z'))
+            {
+                if(currentCharIndex == charPanels.Count - 1 &&
+                    charPanelText[currentCharIndex].text != "" )
+                {
+                    return;
+                }
+                // We've typed a valid input character
+                charPanelText[currentCharIndex].text = inChar.ToString().ToUpper();
+
+                // Increment the index if possible
+                if(currentCharIndex < charPanels.Count - 1)
+                {
+                    currentCharIndex++;
+                }
+            }
+
+            // If the user hits backspace... 
+            else if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                if (currentCharIndex > 0)
+                {
+                    if(currentCharIndex == charPanels.Count - 1 &&
+                        charPanelText[currentCharIndex].text != "")
+                    {
+                        charPanelText[currentCharIndex].text = "";
+                        return;
+                    }
+                    currentCharIndex--;
+                }
+
+                // Clear char panel text
+                charPanelText[currentCharIndex].text = "";
+            }
+
+            // If the user hits enter...
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                doneButton.onClick.Invoke();
+            }
+        }
 	}
 
     public void ShowInput()
     {
-        // Enable the panel, and clean out any previous text
-        inputPanel.gameObject.SetActive(true);
-        inputField.text = "";
-        inputField.characterLimit = expectedWord.Length;
-        placeHolderText.text = "Enter a " + expectedWord.Length + " letter word";
+        // Clean out any previous charPanels and put in new ones.
+        ClearCharPanels();
+
+        // Add in new char panels
+        for(int i = 0; i < expectedWord.Length; i++)
+        {
+            GameObject charP = Instantiate(charPanelPrefab, charPanelGroup, false);
+            charPanels.Add(charP);
+            charPanelText.Add(charP.transform.Find("Elements/Character").GetComponent<Text>());
+        }
+
+        // Enable the panel
+        basePanel.gameObject.SetActive(true);
+        isTyping = true;
 
         // Remove all old listeners, and add the listener to Validate
         // the input 
@@ -45,8 +130,13 @@ public class Keyword : MonoBehaviour {
 
     public void Validate()
     {
-        
-        if(inputField.text.ToLower() == expectedWord.ToLower())
+        // Put together our final validation string
+        foreach(Text text in charPanelText)
+        {
+            enteredKeyword += text.text;
+        }
+
+        if(enteredKeyword.ToUpper() == expectedWord.ToUpper())
         {
             succeed.Invoke();
         }
@@ -55,7 +145,22 @@ public class Keyword : MonoBehaviour {
             fail.Invoke();
         }
 
-        inputPanel.gameObject.SetActive(false);
+        basePanel.gameObject.SetActive(false);
+        isTyping = false;
+        enteredKeyword = "";
     }
 
+    private void ClearCharPanels()
+    {
+        foreach (GameObject go in charPanels)
+        {
+            Destroy(go);
+        }
+        charPanels.Clear();
+
+        // We already destroyed the parent, no need to destroy again.
+        charPanelText.Clear();
+
+        currentCharIndex = 0;
+    }
 }
