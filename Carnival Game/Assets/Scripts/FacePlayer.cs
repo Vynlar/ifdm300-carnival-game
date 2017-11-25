4114f5760a8718ab933dcbc13bf519ecf4add679
Alex <alex.booher@yahoo.com>
2017-11-24 22:00:31 -0700

    NPCs face player

---
+++ Carnival Game/Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour {
+
+    public enum ScaleDirection
+    {
+        Positive,
+        Negative
+    }
+
+    public GameObject player;
+
+    [Tooltip("Should this object be scaled positive when facing left?")]
+    public ScaleDirection scaleWhenLeft = ScaleDirection.Positive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // If player is to the left of this object
+		if(player.transform.position.x < transform.position.x)
+        {
+            if (scaleWhenLeft == ScaleDirection.Positive)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), 
+                    transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x),
+                    transform.localScale.y, transform.localScale.z);
+            }
+        }
+
+        // If the player is to the right of this object
+        else if (player.transform.position.x >= transform.position.x)
+        {
+            if (scaleWhenLeft == ScaleDirection.Positive)
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x),
+                    transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),
+                    transform.localScale.y, transform.localScale.z);
+            }
+        }
+    }
+}
