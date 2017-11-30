d394ec1c4937726b3967bce18acca8a4807f888d
Anna Carey <careya42>
2017-11-29 18:48:20 -0700

    Added dark glows that disappear at appropriate times. Also added Jen Creepy Carnival assets

--- Carnival Game/Assets/Scripts/BossDoor.cs
+++ Carnival Game/Assets/Scripts/BossDoor.cs
@@ -22,5 +22,13 @@ public class BossDoor : MonoBehaviour {
     {
         lockStatus++;
         dialogue.SetDialogState("stage" + lockStatus);
+		
+		//Get rid of the glow around the door when the final puzzle is solved.
+		if(lockStatus == 3)
+		{
+			GameObject DoorGlow = GameObject.Find("BossDoorGlow");
+			EnableDisableScript eds = (EnableDisableScript) DoorGlow.GetComponent("EnableDisableScript");
+			if(eds != null) eds.Disable();
+		}
     }
 }
