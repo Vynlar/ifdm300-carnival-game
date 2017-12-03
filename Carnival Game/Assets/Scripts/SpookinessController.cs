9c8c69c55c8e75f337149b12ba598eb95cb4dace
Vynlar <adrian.aleixandre@gmail.com>
2017-12-02 19:53:58 -0700

    added Jen's spooky animation in

---
+++ Carnival Game/Assets/Scripts/SpookinessController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpookinessController : MonoBehaviour {
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();	
+	}
+
+    public void makeSpooky()
+    {
+        animator.SetBool("isSpooky", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
