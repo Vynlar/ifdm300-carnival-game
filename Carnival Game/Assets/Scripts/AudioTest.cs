0dc8dbbc6b319d4fe1e834c95d520c9af0b591e2
Vincent Crespin <crespinv@unm.edu>
2017-12-04 16:18:00 -0700

    Pushing my stuff

---
+++ Carnival Game/Assets/Scripts/AudioTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class AudioTest : MonoBehaviour {
+
+    public VideoPlayer player;
+    public VideoClip clip;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        player.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        player.SetTargetAudioSource(0, audioSource);
+        player.source = VideoSource.VideoClip;
+        player.clip = clip;
+        player.Play();
+    }
+}
---
+++ Carnival Game/Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditsScript : MonoBehaviour {
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+    public double timer = 0;
+    public double endTime = 263.1;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+        if(timer >= endTime)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        timer += Time.deltaTime;
+    }
+
+}
