dc8da30f5e674c44535cef69a49177a8316c8846
Alexandra Valdez <alvaldez@unm.edu>
2017-12-05 22:10:28 -0700

    poopsoup

---
+++ Carnival Game/Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour {
+    private SpriteRenderer currentSprite;
+    private bool isAdam = false;
+    public Sprite adamTurner;
+    public Sprite puppetMaster;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentSprite = gameObject.GetComponent<SpriteRenderer>();
+        if (Input.GetMouseButtonDown(0))
+        {
+            isAdam = !isAdam;
+            if (isAdam)
+            {
+                currentSprite.sprite = puppetMaster;
+            }
+            if (!isAdam)
+            {
+                currentSprite.sprite = adamTurner;
+            }
+        }
+    }
+}
